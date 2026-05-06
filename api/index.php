17873b00ce4479a51939782448673b1863da2cd7
Usuario <usuario@gestaoigreja.local>
2026-05-06 12:57:11 -0300

    fix: restore standard api/index.php structure and runtime

---
+++ api/index.php
@@ -0,0 +1,3 @@
+<?php
+// Ponte para o index.php original
+require __DIR__ . '/../public/index.php';
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Ponte para o index.php original
-require __DIR__ . '/public/index.php';
