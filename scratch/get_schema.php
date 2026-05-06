5dbcf2556c4480e75bdc4e0c77302d0d4106fbb6
Usuario <usuario@gestaoigreja.local>
2026-05-06 11:48:38 -0300

    chore: remove scratch files from repo

--- scratch/get_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$db = (new App\Config\Database())->getConnection();
-$tables = $db->query('SHOW TABLES')->fetchAll(PDO::FETCH_COLUMN);
-foreach ($tables as $table) {
-    $res = $db->query("SHOW CREATE TABLE `$table`")->fetch();
-    echo $res['Create Table'] . ";\n\n";
-}
--- scratch/list_users.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$db = (new App\Config\Database())->getConnection();
-$users = $db->query('SELECT id, nome, usuario, email FROM usuarios')->fetchAll();
-print_r($users);
